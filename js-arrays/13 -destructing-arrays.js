fdc57e480720f87945a105377061767748d6b10a
EmreGS1905 <hemreakman@outlook.com>
2024-02-26 20:50:40 -0600

    new files

---
+++ js-arrays/13 -destructing-arrays.js
@@ -0,0 +1,12 @@
+const favMovies = [ 'Harry Potter', 'Godfather', 'Lords'];
+
+//Destructuring - not preferred 
+
+
+const credentials = ['TechGlobalSchool', 'Test123', 'techglobal@gmail.com'];
+
+const [ username, password, email] = credentials;
+
+console.log(username);
+console.log(password);
+console.log(email);
---
+++ js-loops/01-loops-intro.js
@@ -0,0 +1,5 @@
+console.log(1);
+
+
+for (let i = 50 ; i >= 0 ; i-=10)
+console.log(i)
