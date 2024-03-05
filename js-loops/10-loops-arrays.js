bf953868b243882d4f4937e0b3be95ada18e3917
EmreGS1905 <hemreakman@outlook.com>
2024-03-04 22:24:11 -0600

    more updates

---
+++ js-loops/10-loops-arrays.js
@@ -0,0 +1,54 @@
+
+const numbers = [3, 4, 7, 3, 2, 2, 7];
+let sum = 0;
+for (let i = 0 ; i < numbers.length ; i++ ) {
+    if (numbers.indexOf(numbers[i]) % 2 === 0) sum+=numbers.indexOf(numbers[i])
+}
+console.log(sum)
+
+const numbers1 = [3, 4, 7, 3, 2, 2, 7];
+let product1 = 1;
+
+for (let i = 1 ; i < numbers1.length; i+=2) {
+    product1*=numbers1[i];
+}
+console.log(product1)
+
+const arr1 = [ 5, 8, 2, 1, 2 ];
+const arr2 = [ 9, 3, 5, 1, 0 ];
+const arr3 = [];
+console.log(arr1);
+console.log(arr2);
+
+for ( let i = 0 ; i < arr1.length; i++) {
+    
+}
+console.log(total)
+
+
+const array1 = [ 5, 8 ];
+const array2 = [ 9, 3, 5, 1, 0 ];
+const array3 = [];
+
+for (let i = 0 ; i < Math.min(array1.length, array2.length) ; i++) {
+    array1.push(array1[i]*array2[i])
+} 
+
+
+const a1 = [10, 3, 7];
+const a2 = [3, 5, 1];
+const a3 = [0, 9, 4];
+
+const a4 =[a1]
+let output = ' '
+const nums = [-1, 1, 3, 0, 2, 6, 8];
+for (i = 0 ; i < nums.length ; i++) {
+    if (nums[i] %2 === 0) output = nums[i]; 
+    
+}
+const nums1 = [-1, 1, 3, 0, 2, 6, 8];
+for (let i = 0 ; i < nums1.length ; i++) {
+    if (nums1[i] % 2 !== 0 > oddNumber) output1 = nums1[i]
+}
+console.log(output1)
+
