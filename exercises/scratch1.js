50547fa5260b49eb572950b464fa818b8db2c9a9
EmreGS1905 <hemreakman@outlook.com>
2024-03-28 19:58:20 -0500

    homework 08 added

--- exercises/scratch1.js
+++ exercises/scratch1.js
@@ -12,3 +12,23 @@ class Cars {
 const car1 = new Cars ('Honda', 'Civic', 'silver', 2023);
 console.log(car1.info());
 
+let vegetables = ["Garlic", "Carrot", "Tomato", "Broccoli"];
+console.log(vegetables.splice(1, 2));
+
+console.log(typeof null)
+
+const a = [2, 5, 7, 8];
+
+console.log(a.splice(1, 0, 'number', 'string'));
+console.log(a)
+
+console.log(a.slice(1,3))
+console.log(a)
+
+const nums = [2, 5, 7, 8];
+console.log(nums.slice(1,3));
+console.log(nums.splice(1,2, '6'));
+console.log(nums);
+
+const nums1 = [2, 2, 5, 7, 8, 8, 10, 10];
+console.log(...new Set (nums1))
---
+++ homework/homework08.js
@@ -0,0 +1,81 @@
+//Task 1 
+const hasLowerCase = (string) => string.toUpperCase() !== string;
+console.log(hasLowerCase(''));
+console.log(hasLowerCase('JAVASCRIPT'));
+console.log(hasLowerCase('hello'));
+console.log(hasLowerCase('125$'));
+console.log(hasLowerCase('a'));
+
+// Task 2
+const noZero = (array) => array.filter( x => x !== 0);
+console.log(noZero([1]));
+console.log(noZero([1, 1, 10]));
+console.log(noZero([0, 1, 10]));
+console.log(noZero([0, 0, 0]));
+console.log(noZero([10, 100, 0]));
+
+// Task 3
+const numberAndSquare = (arr) => arr.map( x => [x , x**2]);
+console.log(numberAndSquare([1, 2, 3]));
+console.log(numberAndSquare([0, 3, -6]));
+console.log(numberAndSquare([1, 4]));
+console.log(numberAndSquare([0, 0, 0]));
+console.log(numberAndSquare([0, 1, -10]));
+
+// Task 4 
+const containsValue = (arr, str) => arr.includes(str);
+console.log(containsValue(['abc', 'foo', 'javascript'] , 'hello'));
+console.log(containsValue(['abc', 'def', '123'] , 'Abc'));
+console.log(containsValue(['abc', 'def', '123', 'Javascript', 'Hello'] , '123'));
+
+// Task 5
+const reverseSentence = (str) => {
+    if(str.split(' ').length < 2) return `There is not enough words`
+    return str.split(' ').reverse().join(' ')
+}
+console.log(reverseSentence('Hello'));
+console.log(reverseSentence('Javascript is fun'));
+console.log(reverseSentence('This is a sentence'));
+
+// Task 6 
+const removeStringSpecialDigits = (str) => { 
+    const arr =str.split('');
+    return arr.filter( x => x >= 'a' && x <= 'z' || x >='A' && x <='Z' || x === ' ').join('');
+}
+console.log(removeStringSpecialDigits('123Javascript #$%is fun'));
+console.log(removeStringSpecialDigits('Cypress'));
+console.log(removeStringSpecialDigits('Automation123#$%'));
+
+// Task 7 
+const removeArraySpecialDigits = (strArr) => {
+    const newArr = strArr.join(' ').split('')
+    return newArr.reduce((str, x) => x >= 'a' && x <= 'z' || x >='A' && x <='Z' || x === ' '? str += x : str, '').split(' ');
+}
+  
+console.log(removeArraySpecialDigits(['123Javascript', '#$%is' ,'fun']));
+console.log(removeArraySpecialDigits(['Cypress', '123$%', '###']));
+console.log(removeArraySpecialDigits(['Automation', '123#$%tool']));
+
+// Task 8 
+const getCommons = (arr1, arr2) => {
+    const arr3 = [];
+    for(const ele of arr1) {
+        for(const word of arr2) {
+            if(word === ele) arr3.push(word)
+        }
+    }return [...new Set(arr3)];
+}
+console.log(getCommons(["Javascript", "is", "fun"], ["abc", "xyz", "123"])); 
+console.log(getCommons(["Javascript", "is", "fun"], ["Javascript", "C#", "Python"])); 	
+console.log(getCommons(["Javascript", "C#", "C#"], ["Python", "C#", "C++"])); 
+
+// Task 9
+const noXInVariables = (arr) => {
+    const newArr = arr.join(' ').split('');
+    return newArr.reduce((str, char) => char.toLowerCase() !== 'x' ? str += char : str, '' ).split(' ').filter(x => x);
+}
+console.log(noXInVariables(['abc', 123, '#$%']));
+console.log(noXInVariables(['xyz', 123, '#$%']));
+console.log(noXInVariables(['x', 123, '#$%']));
+console.log(noXInVariables(['xyXyXy', 'Xx', 'ABC']));
+
